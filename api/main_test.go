089972aaf841f51f0cb5925024443ba0a223e020
NaderSameh <nsag9@hotmail.com>
2023-10-19 16:31:17 +0300

    Adding structured logging + promtail, loki & grafana

--- api/main_test.go
+++ api/main_test.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"net/http"
 	"os"
 	"testing"
 
@@ -13,14 +14,14 @@ func newTestServer(t *testing.T, store db.Store) *Server {
 	server, err := NewServer(store)
 	require.NoError(t, err)
 
-	// server1 := &http.Server{
-	// 	Addr:    ":8080",
-	// 	Handler: server.router,
-	// }
+	server1 := &http.Server{
+		Addr:    ":8080",
+		Handler: server.router,
+	}
 
-	// go server.Start(":8080")
-	// err = server1.Close()
-	// require.NoError(t, err)
+	go server.Start(":8080")
+	err = server1.Close()
+	require.NoError(t, err)
 
 	return server
 }
